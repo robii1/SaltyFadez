78c2ab4b2468c70972531abcd9d631d34eac35b3
robii1 <robinsalte4@gmail.com>
2026-01-18 11:31:58 +0100

    Update AdminPage.js

--- frontend/src/AdminPage.js
+++ frontend/src/AdminPage.js
@@ -23,7 +23,7 @@ import {
 } from "lucide-react";
 import { Link } from "react-router-dom";
 
-const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+const BACKEND_URL = process.env.REACT_APP_API_URL;
 const API = `${BACKEND_URL}/api`;
 
 // Login component
